a93149c0161828774e19d45d96087322d7fb936f
nandani029 <nandanikakani029@gmail.com>
2023-05-03 12:06:14 +0530

    First time commit

---
+++ ML_AI/myfile.py
@@ -0,0 +1,17 @@
+c=40
+def (abc):
+print('good msg')
+
+
+
+
+#dunder
+if__name__=='__main__:b  b  bbb
+print('oh this is')
+a=int(input('enter the first value:'))
+b=int(input('enter the second value:'))
+print(c*(a+b))
+#def function():
+#pass
+#if__name__=='__main__':
+#function()
